5ed008b827fd8fb667e2138b8697b7b04574cff9
Ali Ramazan Yildirim <aliramazanyildirim@gmail.com>
2024-12-28 23:13:10 +0100

    Verbessere die Kontaktliste: Verwende useMemo für die Filterung und füge PropTypes hinzu

--- telefon-buch/src/components/Contacts/List/List.jsx
+++ telefon-buch/src/components/Contacts/List/List.jsx
@@ -1,16 +1,24 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
+import PropTypes from 'prop-types';
 
 const List = ({ contacts }) => {
     const [filterText, setFilterText] = useState("");
 
-    const filtered = contacts.filter((item) => {
-        return Object.keys(item).some((key) =>
-            item[key]
-                .toString()
-                .toLowerCase()
-                .includes(filterText.toLocaleLowerCase())
-        );
-    });
+    const handleFilterTextChange = (e) => {
+        setFilterText(e.target.value);
+    };
+
+    const filtered = useMemo(() => {
+        return contacts.filter((item) => {
+            return Object.keys(item).some((key) =>
+                item[key]
+                    .toString()
+                    .toLowerCase()
+                    .includes(filterText.toLocaleLowerCase())
+            );
+        });
+    }, [contacts, filterText]);
+
     console.log(filtered, "filtered");
 
     return (
@@ -19,19 +27,28 @@ const List = ({ contacts }) => {
                 type="text"
                 placeholder="Filter contact"
                 value={filterText}
-                onChange={(e) => setFilterText(e.target.value)}
+                onChange={handleFilterTextChange}
             />
             <ul className="list">
                 {filtered.map((contact, index) => (
                     <li key={index}>
-                    <span>{contact.fullname}</span>
-                    <span>{contact.phone_number}</span>
+                        <span>{contact.fullname}</span>
+                        <span>{contact.phone_number}</span>
                     </li>
                 ))}
             </ul>
             <p>Total Contacts ({filtered.length})</p>
         </>
     );
+};
+
+List.propTypes = {
+    contacts: PropTypes.arrayOf(
+        PropTypes.shape({
+            fullname: PropTypes.string.isRequired,
+            phone_number: PropTypes.string.isRequired,
+        })
+    ).isRequired,
 };
 
 export default List;
